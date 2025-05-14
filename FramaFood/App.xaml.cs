e47f8fc9bfe31d3d4004dfc406f7f601d2821702
Mabel Cabrera <mcabrera@simetricaconsulting.com>
2025-05-14 13:04:44 -0400

    first commit

---
+++ FramaFood/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FramaFood
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ FramaFood/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FramaFood
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
