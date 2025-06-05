011e0557212f241c729ebbd3b79bb30e12215074
Francisco-97 <Johnkelsantos@gmail.com>
2025-06-05 19:36:02 -0400

    .

--- FramaFoodFE/src/app/components/header-login/header-login.component.ts
+++ FramaFoodFE/src/app/components/header-login/header-login.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { LoginService } from '../../services/login.service';
+import { Router } from 'express';
 
 @Component({
   selector: 'app-header-login',
@@ -8,4 +10,19 @@ import { Component } from '@angular/core';
 })
 export class HeaderLoginComponent {
 
+  private _usuariosService = inject(LoginService)
+  private router = inject(Router)
+
+  ngOnInit(){
+    //this._usuariosService.obtenerUsuarios()
+  }
+
+
+  irPedidoMeseraChef() {
+   this.router.navigate(['/Pedidos/chef']);
+  }
+
+  irMenu() {
+   this.router.navigate(['/Menu']);
+  }
 }
